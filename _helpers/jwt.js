ce697a4028df7e62b0d3b72750663955b944c637
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-01-30 22:03:31 -0300

    /users/foo/

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -5,10 +5,16 @@ module.exports = jwt;
 
 function jwt() {
     const { secret } = config;
+
+    // const { secret } = {
+    //     "secret": "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
+    // };
+
     return expressJwt({ secret }).unless({
         path: [
             // public routes that don't require authentication
             '/users/authenticate'
+///            '/users/foo',
         ]
     });
 }
--- users/users.controller.js
+++ users/users.controller.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userService = require('./user.service');
 
 // routes
+router.post('/foo', foo);
 router.post('/authenticate', authenticate);
 router.get('/', getAll);
 
@@ -10,12 +11,21 @@ module.exports = router;
 
 function authenticate(req, res, next) {
     userService.authenticate(req.body)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
-        .catch(err => next(err));
+        .then(user => 
+            user ? 
+            res.json(user) :
+            res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err =>
+            next(err)
+        );
 }
 
 function getAll(req, res, next) {
     userService.getAll()
         .then(users => res.json(users))
         .catch(err => next(err));
+}
+
+function foo(req, res, next) {
+    res.json({"mensage":"foi"});
 }
