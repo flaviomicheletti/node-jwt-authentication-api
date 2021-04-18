c3bb87c91231417b66298a8f82a5e1939cb3d7ef
Flávio Micheletti <flaviomicheletti@hotmail.com>
2021-04-18 16:36:05 -0300

    detalhes

--- src/users/model.js
+++ src/users/model.js
@@ -1,6 +1,6 @@
 // users hardcoded for simplicity, store in a db for production applications
 const users = [
-    { id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }
+    { id: 1, username: 'email@email.com', password: '1234', firstName: 'John', lastName: 'Dee' }
 ];
 
 module.exports = users;
